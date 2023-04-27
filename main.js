7d9dd7efeaa0ab914088e137746eb85c12c402e2
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-04-28 00:20:56 +0200

    agregando el 0 despues de cada numero en singular

--- main.js
+++ main.js
@@ -23,6 +23,13 @@ let h = new Date().getHours();
 let m = new Date().getMinutes();
 let s = new Date().getSeconds();
 
+//añado el 0 despues de cada numero en singular
+
+h = (h < 10) ? "0" + h : h
+m = (m < 10) ? "0" + m : m
+s = (s < 10) ? "0" + s : s
+
+
 horas.innerHTML = h;
 minutos.innerHTML = m;
 seconds.innerHTML = s;
