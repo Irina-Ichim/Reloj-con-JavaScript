fa3a07fe871c8c43c3a3d3a66893e4608381c721
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-04-27 23:40:07 +0200

    agregando funciones new Date( y funciona el reloj digital

--- main.js
+++ main.js
@@ -13,5 +13,18 @@ hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
 mn.style.transform = `rotateZ(${mm}deg)`;
 sc.style.transform = `rotateZ(${ss}deg)`;
 
+//reloj digital
+let horas = document.getElementById('horas');
+let minutos = document.getElementById('minutos');
+let seconds = document.getElementById('seconds');
+let ampm = document.getElementById('ampm');
+
+let h = new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+
+horas.innerHTML = h;
+minutos.innerHTML = m;
+seconds.innerHTML = s;
 })
 
